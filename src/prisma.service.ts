c6e0978e907f64eb342eebffc977f5f4d26c2034
kirill shubrat <70946028+keeesp@users.noreply.github.com>
2023-07-16 13:31:34 +0300

    Update prisma.service.ts

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+	create(arg0: { email: string; password: string; contacts: undefined[] }) {
+		throw new Error('Method not implemented.')
+	}
+	save(newUser: any) {
+		throw new Error('Method not implemented.')
+	}
+	findOne(arg0: { where: { id: any } }) {
+		throw new Error('Method not implemented.')
+	}
 	async onModuleInit() {
 		await this.$connect()
 	}
